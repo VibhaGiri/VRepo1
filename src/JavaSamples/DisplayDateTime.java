7f27e2d8913bda0a31cff71c026ab6174ce1939b
vibha <vibha.giri87@gmail.com>
2023-11-11 16:57:53 -0500

    First commit

---
+++ src/JavaSamples/DisplayDateTime.java
@@ -0,0 +1,12 @@
+package JavaSamples;
+
+import java.time.LocalDateTime;
+
+public class DisplayDateTime
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Current Date and Time is " +LocalDateTime.now());
+
+    }
+}
